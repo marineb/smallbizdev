4b5309c4cabbda037832f5d95c5d53962e49357d
Marine Boudeau <m14868@gmail.com>
2014-09-13 14:27:52 -0400

    First commit

---
+++ js/controller.js
@@ -0,0 +1,31 @@
+var myAppModule = angular.module('profitly', ['ngRoute']);
+
+function feelioRouteConfig($routeProvider, $locationProvider) {
+  $routeProvider.
+  when ('/', {
+    templateUrl: '/views/home.html'
+  }).
+  otherwise ({
+    redirectTo: '/'
+  });
+  
+  $locationProvider.html5Mode(true);  
+}
+
+myAppModule.controller('MainController', function($scope, $route, $routeParams, $location) {
+     $scope.$route = $route;
+     $scope.$location = $location;
+     $scope.$routeParams = $routeParams;
+ })
+
+//console.log(route);
+//console.log(location);
+
+myAppModule.config(feelioRouteConfig);
+
+// Make sure GA passes the right data
+myAppModule.run(function ($rootScope, $location) {
+    $rootScope.$on('$routeChangeSuccess', function(){
+      ga('send', 'pageview', $location.path());
+    });
+});
